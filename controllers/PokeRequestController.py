25c222ba11265c2aac2663c2d690b73025888f89
andre <andre.herrera@unah.hn>
2025-04-19 22:24:30 -0600

    último video

--- controllers/PokeRequestController.py
+++ controllers/PokeRequestController.py
@@ -6,6 +6,7 @@ from fastapi import HTTPException
 from models.PokeRequest import PokeRequest
 from utils.database import execute_query_json
 from utils.AQueue import AQueue
+from utils.ABlob import ABlob
 
 #configurar el logging
 logging.basicConfig(level=logging.INFO)
@@ -27,6 +28,8 @@ async def select_pokemon_request(id: int) -> dict:
 async def update_pokemon_request(pokemon_request: PokeRequest) -> dict:
     try:
         query = "exec pokequeue.update_poke_request ?, ?, ? "
+        if not pokemon_request.url:
+            pokemon_request.url = "";
         params = (pokemon_request.id, pokemon_request.status, pokemon_request.url,)
         result = await execute_query_json(query, params, True)
         result_dict = json.loads(result)
@@ -54,3 +57,25 @@ async def insert_pokemon_request(pokemon_request: PokeRequest) -> dict:
     except Exception as e:
         logger.error(f"Error inserting report request: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+
+async def get_all_requests() -> dict:
+    query = """
+                SELECT 
+                    r.id as ReportId,
+                    , s.decription as Status
+                    , r.type as PokemonType
+                    , r.url
+                    , r.created
+                    , r.updated
+                FROM pokequeue.requests r
+                INNER JOIN pokequeue.status s
+                ON r.id_status = s.id
+            """
+    result = await execute_query_json( query )
+    result_dict = json.loads(result)
+    blob = ABlob()
+    for record in result_dict:
+        id = record['ReportId']
+        record['url'] = f"{record['url']}?{blob.generate_sas(id)}"
+    return result_dict
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.database import execute_query_json
-from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request
+from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request, get_all_requests
 from models.PokeRequest import PokeRequest
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -24,6 +33,10 @@ async def version():
 async def select_request(id: int):
     return await select_pokemon_request( id )
 
+@app.get("/api/request")
+async def select_all_reques():
+    return await get_all_requests()
+
 @app.post("/api/request")
 async def create_request(pokemon_request: PokeRequest):
     return await insert_pokemon_request(pokemon_request)
@@ -31,6 +44,7 @@ async def create_request(pokemon_request: PokeRequest):
 @app.put("/api/request")
 async def update_request(pokemon_request: PokeRequest):
     return await update_pokemon_request(pokemon_request)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
